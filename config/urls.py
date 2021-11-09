cd5feef174fd08e180a700aa4e02222cc8a5a700
EvZhdan <ev107@mail.ru>
2021-11-09 21:59:53 +0300

    Dev and deployment versions

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""landing URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -20,9 +20,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from crm import views
+from settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('thanks/', views.thanks_page, name = "thanks_page"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
